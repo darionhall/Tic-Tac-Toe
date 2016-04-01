50ba0536fbc509bead265b6bdfb2adde1c3d52e1
Darion Hall <darionhall@gmail.com>
2016-04-01 14:02:32 -0400

    created gameboard

--- JavaSciptTTT/app.js
+++ JavaSciptTTT/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
 
-  console.log("loaded");
+  // console.log("loaded"); //
+
+  var $gameCells = $('.cell');
+
+  $('.cell').one('click', function(){
+//one for the click to only take once or run this $('.cell').on('click', function(){
+  //  console.log("Board Clicked!");
+
+    console.log(this.id);
+  })
+
+
 
 
 });
