63e052280ce499a90cd70b25f78d57f2093e5421
Darion Hall <darionhall@gmail.com>
2016-04-01 18:11:55 -0400

    gameboard created, with click function, and move count.

--- JavaSciptTTT/app.js
+++ JavaSciptTTT/app.js
@@ -1,17 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function(){ //first step
 
   // console.log("loaded"); //
 
-  var $gameCells = $('.cell');
 
-  $('.cell').one('click', function(){
-//one for the click to only take once or run this $('.cell').on('click', function(){
-  //  console.log("Board Clicked!");
+  var $gameCells = $('.cell'); // third step
+  var moves = ["","","","","","","","",""];  //fourth step, create array of moves
+  var count = 1;
+  var turn = 'X';
 
-    console.log(this.id);
-  })
+  $($gameCells).one('click', function(){
+      $(this).html(turn);
+      moves[this.id] = turn; //fifth checks the array of moves in order
+
+    count++; //sixth step
+
+    turn = (turn == 'X') ? turn = 'O' : turn = 'X';
+
+    console.log(moves, count);
+  });
 
 
 
 
 });
+
+// if ()
